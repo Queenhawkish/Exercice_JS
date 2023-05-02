7eacd805c862afc858fb79fee5c0850fadcc9fe2
Aglaé <aglae.michot@gmail.com>
2023-05-02 14:12:47 +0200

    4tab

---
+++ Tableaux/ex5.js
@@ -0,0 +1,7 @@
+let tab = [
+    [["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+["Lundi", "Mardi", "Mercredi", "Jeudi", "Vencredi", "Samedi", "Dimanche"]],
+[["January", "February", "March" , "April" , "May" , "June" , "July" , "August" , "September" , "October" , "November" , "December"],
+["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saterday", "Sunday"],
+]]
+console.log(tab)
