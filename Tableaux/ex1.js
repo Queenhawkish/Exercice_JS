a9db6c104936eed2c8965fe6cbc753bd1dd8f060
Aglaé <aglae.michot@gmail.com>
2023-05-02 13:18:25 +0200

    tableau

---
+++ Tableaux/ex1.js
@@ -0,0 +1,14 @@
+let tab = new Array("Janvier")
+tab [1] = "Février"
+tab [2] = "Mars"
+tab [3] = "Avril"
+tab [4] = "Mai"
+tab [5] = "Juin"
+tab [6] = "Juillet"
+tab [7] = "Août"
+tab [8] = "Septembre"
+tab [9] = "Octobre"
+tab [10] = "Novembre"
+tab [11] = "Décembre"
+let month = ["Janvier" , "Février" , "Mars" , "Avril" , "Mai" , "Juin" , "Juillet" , "Août" , "Septembre" , "Octobre" , "Novembre" , "Décembre"]
+console.table(month)
