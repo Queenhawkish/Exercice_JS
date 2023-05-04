bc5892d0a44fd81a77fa9658c4752dc65d4ff69a
Aglaé <aglae.michot@gmail.com>
2023-05-04 08:39:02 +0200

    guessnb

--- Fonction/Exercice/ex2/assets/js/Script.js
+++ Fonction/Exercice/ex2/assets/js/Script.js
@@ -1,12 +1,31 @@
 const nbtoguess = 12
 
-let input = document.getElementById("input").value
-
 let send = document.getElementById("button")
 
-tentative = 0
+let mess = document.getElementById("messages")
 
-send.addEventListener("click" , function(){
+let ancien = document.getElementById("ancien")
+
+let tentative = 0
+
+
+
+function comparer(input) {
+    if (input == nbtoguess) {
+        mess.innerText = `Bravo vous avez réussit après ${tentative} de tentavive !`
+    } else if (input < nbtoguess) {
+        mess.innerText = `C'est un nombre plus grand !`
+        ancien.innerText += input + " ; "
+    } else if (input > nbtoguess) {
+        mess.innerText = `C'est un nombre plus petit !`
+        ancien.innerText += input + " ; "
+    } else {
+        mess.innerText = "Veuillez saisir un paramètre valide"
+    }
+}
+
+send.addEventListener("click", function () {
+    let input = document.getElementById("input").value
+    comparer(input)
     tentative++
-    console.log(tentative)
 })
