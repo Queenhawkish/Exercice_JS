148119f2eda5c1701ddc222d9352368997baa9fe
Aglaé <aglae.michot@gmail.com>
2023-05-09 17:18:56 +0200

    almost

--- Fonction/Exercice/ex4/assets/js/Script.js
+++ Fonction/Exercice/ex4/assets/js/Script.js
@@ -32,138 +32,189 @@ let point = document.getElementById("point")
 
 let equal = document.getElementById("equal")
 
-let op = document.getElementById("op")
+let operation = document.getElementById("op")
 
 let result = document.getElementById("result")
-
-let table = [seven, eight, nine, divid, four, five, six, multiple, one, two, three, minus, zero, point, plus, equal]
-
-console.table(table)
 
 let nb1 = document.getElementById("nb1")
 
 let nb2 = document.getElementById("nb2")
 
+let results = ""
 
-function calculator(a, b, op) {
-    let results = "";
-    switch (op) {
-        case '+':
-            results = a + b;
-        case '-':
-            results = a - b;
-        case '*':
-            results = a * b;
-        case '/': {
-            if (b !== 0)
-                results = a / b;
-            else
-                results = "Infinity";
-        }
-    }
-}
+divid.addEventListener("click", function () {
+    operation.innerText = divid.innerText
+})
+
+multiple.addEventListener("click", function () {
+    operation.innerText = multiple.innerText
+})
+
+minus.addEventListener("click", function () {
+    operation.innerText = minus.innerText
+})
+
+plus.addEventListener("click", function () {
+    operation.innerText = plus.innerText
+})
+
 ac.addEventListener("click", function () {
     nb1.innerText = ""
-    op.innerText = ""
+    operation.innerText = ""
     nb2.innerText = ""
     result.innerText = ""
 })
 
 seven.addEventListener("click", function () {
-    nb1.innerText += seven.innerText
-    if(divid.addEventListener("click")){
+
+    if (operation.innerText == divid.innerText ||
+        operation.innerText == multiple.innerText ||
+        operation.innerText == minus.innerText ||
+        operation.innerText == plus.innerText) {
         nb2.innerText += seven.innerText
+    } else {
+        nb1.innerText += seven.innerText
     }
 })
 
 eight.addEventListener("click", function () {
-    nb1.innerText += eight.innerText
-    if(divid.addEventListener("click")){
+
+    if (operation.innerText == divid.innerText ||
+        operation.innerText == multiple.innerText ||
+        operation.innerText == minus.innerText ||
+        operation.innerText == plus.innerText) {
         nb2.innerText += eight.innerText
+    } else {
+        nb1.innerText += eight.innerText
     }
 })
-
 nine.addEventListener("click", function () {
-    nb1.innerText += nine.innerText
-    if(divid.addEventListener("click")){
+
+    if (operation.innerText == divid.innerText ||
+        operation.innerText == multiple.innerText ||
+        operation.innerText == minus.innerText ||
+        operation.innerText == plus.innerText) {
         nb2.innerText += nine.innerText
+    } else {
+        nb1.innerText += nine.innerText
     }
 })
 
 four.addEventListener("click", function () {
-    nb1.innerText += four.innerText
-    if(divid.addEventListener("click")){
+
+    if (operation.innerText == divid.innerText ||
+        operation.innerText == multiple.innerText ||
+        operation.innerText == minus.innerText ||
+        operation.innerText == plus.innerText) {
         nb2.innerText += four.innerText
+    } else {
+        nb1.innerText += four.innerText
     }
 })
 
 five.addEventListener("click", function () {
-    nb1.innerText += five.innerText
-    if(divid.addEventListener("click")){
+
+    if (operation.innerText == divid.innerText ||
+        operation.innerText == multiple.innerText ||
+        operation.innerText == minus.innerText ||
+        operation.innerText == plus.innerText) {
         nb2.innerText += five.innerText
+    } else {
+        nb1.innerText += five.innerText
     }
 })
 
 six.addEventListener("click", function () {
-    nb1.innerText += six.innerText
-    if(divid.addEventListener("click")){
+
+    if (operation.innerText == divid.innerText ||
+        operation.innerText == multiple.innerText ||
+        operation.innerText == minus.innerText ||
+        operation.innerText == plus.innerText) {
         nb2.innerText += six.innerText
+    } else {
+        nb1.innerText += six.innerText
     }
 })
 
 one.addEventListener("click", function () {
-    nb1.innerText += one.innerText
-    if(divid.addEventListener("click")){
+
+    if (operation.innerText == divid.innerText ||
+        operation.innerText == multiple.innerText ||
+        operation.innerText == minus.innerText ||
+        operation.innerText == plus.innerText) {
         nb2.innerText += one.innerText
+    } else {
+        nb1.innerText += one.innerText
     }
 })
 
 two.addEventListener("click", function () {
-    nb1.innerText += two.innerText
-    if(divid.addEventListener("click")){
+
+    if (operation.innerText == divid.innerText ||
+        operation.innerText == multiple.innerText ||
+        operation.innerText == minus.innerText ||
+        operation.innerText == plus.innerText) {
         nb2.innerText += two.innerText
+    } else {
+        nb1.innerText += two.innerText
     }
 })
 
 three.addEventListener("click", function () {
-    nb1.innerText += three.innerText
-    if(divid.addEventListener("click")){
+
+    if (operation.innerText == divid.innerText ||
+        operation.innerText == multiple.innerText ||
+        operation.innerText == minus.innerText ||
+        operation.innerText == plus.innerText) {
         nb2.innerText += three.innerText
+    } else {
+        nb1.innerText += three.innerText
     }
 })
 
 zero.addEventListener("click", function () {
-    nb1.innerText += zero.innerText
-    if(divid.addEventListener("click")){
+
+    if (operation.innerText == divid.innerText ||
+        operation.innerText == multiple.innerText ||
+        operation.innerText == minus.innerText ||
+        operation.innerText == plus.innerText) {
         nb2.innerText += zero.innerText
+    } else {
+        nb1.innerText += zero.innerText
     }
 })
 
 point.addEventListener("click", function () {
-    nb1.innerText += point.innerText
-    if(divid.addEventListener("click")){
+
+    if (operation.innerText == divid.innerText ||
+        operation.innerText == multiple.innerText ||
+        operation.innerText == minus.innerText ||
+        operation.innerText == plus.innerText) {
         nb2.innerText += point.innerText
+    } else {
+        nb1.innerText += point.innerText
     }
 })
 
-divid.addEventListener("click", function () {
-    op.innerText += divid.innerText
+function calculator(nb1 , nb2 , operation) {
+    console.log(nb1 , nb2 , operation)
+    switch (operation) {
+        case '+':
+            results = +nb1.innerText + +nb2.innerText;
+        case '-':
+            results = +nb1.innerText - +nb2.innerText;
+        case '*':
+            results = +nb1.innerText * +nb2.innerText;
+        case '/': {
+            if (b !== 0)
+                results = +nb1.innerText / +nb2.innerText;
+            else
+                results = "infini";
+        }
+    }
+}
+
+equal.addEventListener("click", function () {
+    calculator(nb1 , nb2 , operation)
+    result.innerText = results
 })
-
-multiple.addEventListener("click", function () {
-    op.innerText += multiple.innerText
-})
-
-minus.addEventListener("click", function () {
-    op.innerText += minus.innerText
-})
-
-plus.addEventListener("click", function () {
-    op.innerText += plus.innerText
-})
-
-
-
-// equal.addEventListener("click" , function() {
-//     result.innerText = equal.innerText
-// })
